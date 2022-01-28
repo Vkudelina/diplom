a5d4108a7e3c3967b091d17e9917cfa44249d08c
Vkudelina <valentina.kudelina@hubbiton.info>
2022-01-28 23:37:48 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import News from "./components/News.vue"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: News },
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  router,
 }).$mount('#app')
